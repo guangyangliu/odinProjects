5e1b3b0deb52c412e224f74bc46d4bec1f054afd
liuguangyang <liuguangyang1994@gmail.com>
2024-03-24 11:57:25 +0800

    Finish exercise 06 leapYears. My solution is more verbose than provided solution.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //if divisible, give 1, else 0.
+    let divisibleBy400 = !(year%400);
+    let divisibleBy100 = !(year%100);
+    let divisibleBy4 = !(year%4);
+    if (divisibleBy400) {
+        return true;
+    } else if (divisibleBy100) {
+        return false;
+    } else if (divisibleBy4) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
