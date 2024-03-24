b0fbcbb50e308a943b9d326823a324b8d66ea7c7
liuguangyang <liuguangyang1994@gmail.com>
2024-03-24 11:11:02 +0800

    Finish exercise 04 -removeFromArray.define help function elementNotEqual and use filter function to return a filtered array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeElements) {
+    for (let removeElement of removeElements) {
+        //help function to filter not euqal elements.
+        let elementNotEqual = function(arrayElement) {
+            return arrayElement !== removeElement;
+        };
+        array = array.filter(elementNotEqual);
+    }
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
