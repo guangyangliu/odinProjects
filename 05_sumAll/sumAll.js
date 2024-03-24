fc735fb134b1ec037e55ac1dc5c40cf2d750ed59
liuguangyang <liuguangyang1994@gmail.com>
2024-03-24 11:46:03 +0800

    Finish exercise 05 -summAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(typeof(start) !== typeof(end) || 
+        typeof(start) !== typeof(1) || 
+        start < 0 || 
+        end < 0) {
+            return 'ERROR';
+        }
+    let sum = 0;
+    if(start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    while(start<= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
