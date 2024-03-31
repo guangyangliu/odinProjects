60b787ed95b4a9b6d8e7ee56345dea82d60969fa
liuguangyang <liuguangyang1994@gmail.com>
2024-03-31 18:59:08 +0800

    Finish exercises of last section(Object Basics) of Foundations (of the Odin Project) in a cafe of Chula school in Thailand.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(typeof n === "string") {
+        n = parseInt(n);
+    }
+    if(n<0) {
+        return "OOPS";
+    }
+    return n<=1? n : fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
