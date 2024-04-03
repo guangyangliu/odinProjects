673d5c67b0449b775b3800d116aa958756f86e51
liuguangyang <liuguangyang1994@gmail.com>
2024-04-03 11:04:21 +0800

    Finish last project calculator of foundations of the odin project.

--- calculator.js
+++ calculator.js
@@ -21,9 +21,9 @@ function operate(operator, firstNumber, secondNumber) {
         case "+" :
             return add(firstNumber, secondNumber);
         case "-" :
-            return multiply(firstNumber, secondNumber);
+            return subtract(firstNumber, secondNumber);
         case "*" :
-            return divide(firstNumber, secondNumber);
+            return multiply(firstNumber, secondNumber);
         case "/" :
             return divide(firstNumber, secondNumber);
         default:
@@ -31,21 +31,61 @@ function operate(operator, firstNumber, secondNumber) {
     }
 }
 
+let displayDiv = document.querySelector("#display");
+
 function display() {
     let buttons = document.querySelector("#buttons");
     buttons.addEventListener('click', (event)=> {
         let target = event.target;
-        let displayDiv = document.querySelector("#display");
         let buttonContent = target.textContent;
-        if (buttonContent.length == 1) {
-            if(buttonContent === "C") {
-                displayDiv.textContent = "diplay area";
-            } else if(buttonContent === "=") {
-                displayDiv.textContent = "solution";
+        let buttonClasslist = target.classList;
+        if(buttonClasslist.contains("clear")) {
+                firstNumber = undefined;
+                secondNumber = undefined;
+                operator = undefined;
+                displayDiv.textContent = "0";
+        }
+
+        if(buttonClasslist.contains("eval")) {
+            if(secondNumber !== undefined) {
+                eval();
             } else {
-                displayDiv.textContent = buttonContent;
+                displayDiv.textContent = "Error!";
             }
         }
+
+        if(buttonClasslist.contains("number")) {
+            if(operator === undefined) {
+                firstNumber = firstNumber?Number("0"+firstNumber+buttonContent):Number(buttonContent);
+                displayDiv.textContent = firstNumber;
+            } else {
+                secondNumber = secondNumber?Number("0"+secondNumber+buttonContent):Number(buttonContent);
+                displayDiv.textContent = secondNumber;
+            }
+            
+        }
+
+        if(buttonClasslist.contains("operator")) {
+            if(secondNumber !== undefined) {
+                eval();
+            } else if(firstNumber !== undefined) {
+                displayDiv.textContent = buttonContent;
+            }
+            operator = buttonContent;
+        }
     });
+}
+
+function eval() {
+    if(operator === "/" && secondNumber === 0) {
+        displayDiv.textContent = "Can't divide by 0";
+        return;
+    }
+    let result = operate(operator, firstNumber, secondNumber);
+    result = Number(result.toFixed(8));
+    firstNumber = result;
+    secondNumber = undefined;
+    operator = undefined;
+    displayDiv.textContent = result;
 }
 display();
