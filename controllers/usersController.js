f36ad86b7d76b55dc65618b51aabcb6cb1d3c330
liuguangyang <liuguangyang1994@gmail.com>
2024-08-01 21:20:20 +0800

    Initial commit of homework aboutforms and data handling in express.

---
+++ controllers/usersController.js
@@ -0,0 +1,86 @@
+// controllers/usersController.js
+const asyncHandler = require("express-async-handler");
+const usersStorage = require("../storages/usersStorage");
+
+exports.usersListGet = asyncHandler(async (req, res) => {
+  res.render("index", {
+    title: "User list",
+    users: usersStorage.getUsers(),
+  });
+});
+
+exports.usersCreateGet = asyncHandler(async (req, res) => {
+  res.render("createUser", {
+    title: "Create user",
+  });
+});
+
+exports.usersCreatePost = asyncHandler(async (req, res) => {
+  const { firstName, lastName } = req.body;
+  usersStorage.addUser({ firstName, lastName });
+  res.redirect("/");
+});
+
+// This just shows the new stuff we're adding to the existing contents
+const {body, validationResult} = require("express-validator");
+const alphaErr = "must only contain letters";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validatorUser = [
+    body("firstName").trim().isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({min: 1, max: 10}).withMessage(`First name ${lengthErr}`),
+    body("lastName").trim()
+        .isAlpha().withMessage(`Last name ${alphaErr}`)
+        .isLength({min: 1, max: 10}).withMessage(`Last name ${lengthErr}`),
+]
+
+
+exports.usersCreatePost = [
+    validatorUser,
+    asyncHandler(async(req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).render("createUser", {
+                title: "Create user",
+                errors: errors.array(),
+            });
+        }
+        const {firstName, lastName} = req.body;
+        usersStorage.addUser({firstName, lastName});
+        res.redirect("/");
+    })
+]
+
+exports.usersUpdateGet = asyncHandler(async (req, res) => {
+    const user = usersStorage.getUser(req.params.id);
+    res.render("updateUser", {
+      title: "Update user",
+      user: user,
+    });
+  });
+  
+  exports.usersUpdatePost = [
+    validatorUser,
+    asyncHandler(async (req, res) => {
+      const user = usersStorage.getUser(req.params.id);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("updateUser", {
+          title: "Update user",
+          user: user,
+          errors: errors.array(),
+        });
+      }
+      const { firstName, lastName } = req.body;
+      usersStorage.updateUser(req.params.id, { firstName, lastName });
+      res.redirect("/");
+    })
+  ];
+  
+
+  // Tell the server to delete a matching user, if any. Otherwise, respond with an error.
+exports.usersDeletePost = asyncHandler(async (req, res) => {
+    usersStorage.deleteUser(req.params.id);
+    res.redirect("/");
+  });
+  
