a99c290158d57cbad28461874eb94b0f75f66723
liuguangyang <liuguangyang1994@gmail.com>
2024-08-07 15:32:01 +0800

    Show car type in left and car name in right defaultly.

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//use assets folder
+const assetPath = path.join(__dirname, 'public');
+app.use(express.static(assetPath));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
