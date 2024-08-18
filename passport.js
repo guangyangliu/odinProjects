a0ea0398a9a3c4e73d7bf3f18b374873c20d3b0a
liuguangyang <liuguangyang1994@gmail.com>
2024-08-18 15:46:48 +0800

    Fix bug: passport not work. Bug reason: login form didn't has username and password value. It use custom fields: uname, pwd.

--- passport.js
+++ passport.js
@@ -4,21 +4,21 @@ const bcrypt = require('bcryptjs');
 const model = require('./model/querries');
 
 
-passport.use(new LocalStrategy(
-    async (username, password, done) => {
-        try {
-        console.log(111);
-        const user = await model.getUserByUsername(username);
-        if(!user) return done(null, false);
-        console.log(`input${password}, db:${user.password}`);
-        const match = await bcrypt.compare(password, user.password);
-        if(!match) return done(null, false);
-        return done(null, user);
-        } catch (error) {
-            return done(error);
-        }
+const verify = async (username, password, done) => {
+    try {
+    console.log(111);
+    const user = await model.getUserByUsername(username);
+    if(!user) return done(null, false);
+    console.log(`input${password}, db:${user.password}`);
+    const match = await bcrypt.compare(password, user.password);
+    if(!match) return done(null, false);
+    return done(null, user);
+    } catch (error) {
+        return done(error);
     }
-));
+};
+const strategy = new LocalStrategy(verify);
+passport.use(strategy);
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
